38a952777a702879c6781486247ff22f40efbb11
Reynold Chan <reynold.ch@gmail.com>
2022-02-25 15:38:16 -0500

    Clean Up For Testing with CSI

--- ConnectorETABS/ConnectorETABSv18/cPlugin.cs
+++ ConnectorETABS/ConnectorETABSv18/cPlugin.cs
@@ -8,17 +8,11 @@ using DesktopUI2.ViewModels;
 using DesktopUI2.Views;
 using System.Timers;
 using System.Diagnostics;
-
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.ReactiveUI;
-
-
 using CSiAPIv1;
-
 using Speckle.Core.Logging;
-using Speckle.ConnectorETABS.Util;
-using Speckle.ConnectorETABS.UI;
 using System.Reflection;
 using System.IO;
 
@@ -35,38 +29,38 @@ namespace SpeckleConnectorETABS
 
     public static Window MainWindow { get; private set; }
 
-    public static ConnectorBindingsETABS Bindings { get; set; }
+    //public static ConnectorBindingsETABS Bindings { get; set; }
 
     public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<DesktopUI2.App>()
-    .UsePlatformDetect()
-    .With(new SkiaOptions { MaxGpuResourceSizeBytes = 8096000 })
-    .With(new Win32PlatformOptions { AllowEglInitialization = true, EnableMultitouch = false })
-    .LogToTrace()
-    .UseReactiveUI();
-
+      .UsePlatformDetect()
+      .With(new SkiaOptions {MaxGpuResourceSizeBytes = 8096000})
+      .With(new Win32PlatformOptions {AllowEglInitialization = true, EnableMultitouch = false})
+      .LogToTrace()
+      .UseReactiveUI();
 
 
     public static void CreateOrFocusSpeckle()
     {
       if (MainWindow == null)
       {
-
         BuildAvaloniaApp().Start(AppMain, null);
+        //var var = AppBuilder.Configure<DesktopUI2.App>()
+        //  .UsePlatformDetect()
+        //  .With(new SkiaOptions {MaxGpuResourceSizeBytes = 8096000});
+        //.With(new Win32PlatformOptions {AllowEglInitialization = true, EnableMultitouch = false});
+        //var.Start(AppMain, null);
+
+
       }
 
       MainWindow.Show();
       MainWindow.Activate();
-
-
     }
 
     private static void AppMain(Application app, string[] args)
     {
       var viewModel = new MainWindowViewModel();
-      MainWindow = new MainWindow
-      {
-        DataContext = viewModel
-      };
+      MainWindow = new MainWindow {DataContext = viewModel};
 
       app.Run(MainWindow);
       //Task.Run(() => app.Run(MainWindow));
@@ -78,22 +72,23 @@ namespace SpeckleConnectorETABS
       CreateOrFocusSpeckle();
 
 
-      var processes = Process.GetProcesses();
-      IntPtr ptr = IntPtr.Zero;
-      foreach (var process in processes)
-      {
-        if (process.ProcessName.ToLower().Contains("etabs"))
-        {
-          ptr = process.MainWindowHandle;
-          break;
-        }
-      }
-      if (ptr != IntPtr.Zero)
-      {
-        //Application.Current.MainWindow.Closed += SpeckleWindowClosed;
-        MainWindow.Closed += SpeckleWindowClosed;
-        MainWindow.Closing += SpeckleWindowClosed;
-      }
+      //var processes = Process.GetProcesses();
+      //IntPtr ptr = IntPtr.Zero;
+      //foreach (var process in processes)
+      //{
+      //  if (process.ProcessName.ToLower().Contains("sap2000"))
+      //  {
+      //    ptr = process.MainWindowHandle;
+      //    break;
+      //  }
+      //}
+
+      //if (ptr != IntPtr.Zero)
+      //{
+      //  //Application.Current.MainWindow.Closed += SpeckleWindowClosed;
+      //  MainWindow.Closed += SpeckleWindowClosed;
+      //  MainWindow.Closing += SpeckleWindowClosed;
+      //}
       //}
       //catch
       //{
@@ -118,14 +113,14 @@ namespace SpeckleConnectorETABS
 
     public int Info(ref string Text)
     {
-      Text = "This is a Speckle plugin for ETABS";
+      Text = "This is a Speckle plugin for CSI Products";
       return 0;
     }
 
-    static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+    public static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
     {
       Assembly a = null;
-      var name = args.Name.Split(',')[0];
+      var name = args.Name.Split(',')[ 0 ];
       string path = Path.GetDirectoryName(typeof(cPlugin).Assembly.Location);
 
       string assemblyFile = Path.Combine(path, name + ".dll");
@@ -141,12 +136,17 @@ namespace SpeckleConnectorETABS
       cSapModel model;
       pluginCallback = ISapPlugin;
       AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(OnAssemblyResolve);
+      //AppDomain.CurrentDomain.TypeResolve += new ResolveEventHandler(OnAssemblyResolve);
+      //AppDomain.CurrentDomain.ResourceResolve += new ResolveEventHandler(OnAssemblyResolve);
+      //AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(OnAssemblyResolve);
+
+      model = SapModel;
 
       try
       {
-        cHelper helper = new Helper();
-        var etabsObject = helper.GetObject("CSI.SAP2000.API.SapObject");
-        model = etabsObject.SapModel;
+        //cHelper helper = new Helper();
+        //var etabsObject = helper.GetObject("CSI.ETABS.API.ETABSObject");
+        //model = etabsObject.SapModel;
       }
       catch
       {
@@ -157,14 +157,22 @@ namespace SpeckleConnectorETABS
       try
       {
         OpenOrFocusSpeckle(model);
-        SelectionTimer = new Timer(2000) { AutoReset = true, Enabled = true };
+        //var windows = new Button();
+        //var reactiveUI = new ReactiveUI.ReactiveObject();
+        //var Avalonia = new Avalonia.ReactiveUI.ViewModelViewHost();
+        //var DUI2 = new DesktopUI2.App();
+        //var avaolniaVis = Avalonia.Visuals.Media.Imaging.BitmapBlendingMode.Destination;
+        //var mainWindow = new MainWindowViewModel();
+        //var app = new AppBuilder();
+
+        SelectionTimer = new Timer(2000) {AutoReset = true, Enabled = true};
         SelectionTimer.Elapsed += SelectionTimer_Elapsed;
         SelectionTimer.Start();
       }
 
       catch (Exception e)
       {
-
+        throw e;
         ISapPlugin.Finish(0);
         return;
       }
